33514d98b869030a2d48fa953518e57646db72dd
ThanuraSilva <thanurasilva1989@gmail.com>
2022-03-13 14:17:21 +0530

    Data saved as the given logics

--- src/main/java/lk/ijse/dep8/control/MainInterfaceControl.java
+++ src/main/java/lk/ijse/dep8/control/MainInterfaceControl.java
@@ -6,6 +6,9 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import lk.ijse.dep8.util.Customer;
 
 import java.io.*;
@@ -16,15 +19,34 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
 public class MainInterfaceControl {
+
     private final Path sourcePath = Paths.get("dataSource/customers.dep8");
+    private Path defaultImage = Paths.get("assets/MaleIcon.png");
+
+
     public TextField txtId;
+
     public TextField txtName;
+
     public TextField txtAddress;
+
     public Button btnSave;
+
     public Button btnReset;
+
     public TableView<Customer> tblCustomer;
 
-    public void initialize() throws IOException {
+    public TextField txtProPic;
+    public Button btnBrowse;
+    public ImageView imgProfile;
+    public RadioButton rbtMale;
+    public RadioButton rbtFemale;
+
+    private Button btnDelete;
+    Path imagePath;
+
+
+    public void initialize(){
 
         initDataSource();
 
@@ -32,23 +54,44 @@ public class MainInterfaceControl {
         tblCustomer.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
         tblCustomer.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
 
-        TableColumn<Customer, Button> colOpt =
-                (TableColumn<Customer, Button>) tblCustomer.getColumns().get(3);
-
-        colOpt.setCellValueFactory(param -> {
-            Button btnDel = new Button("Delete");
-
-            btnDel.setOnAction((event -> tblCustomer.getItems().remove(param.getValue())));
-            return new ReadOnlyObjectWrapper<>(btnDel);
+        TableColumn<Customer, ImageView> colProPic =
+                (TableColumn<Customer, ImageView>) tblCustomer.getColumns().get(3);
+        colProPic.setCellValueFactory(param -> {
+            byte[] profilePic = param.getValue().getProfilePic();
+            ByteArrayInputStream bis = new ByteArrayInputStream(profilePic);
+            ImageView imgVw = new ImageView(new Image(bis));
+            imgVw.setFitWidth(80);
+            imgVw.setFitHeight(80);
+            return new ReadOnlyObjectWrapper<>(imgVw);
         });
 
+        TableColumn<Customer, Button> colOpt =
+                (TableColumn<Customer, Button>) tblCustomer.getColumns().get(4);
+
+        colOpt.setCellValueFactory(param -> {
+             btnDelete = new Button("Delete");
+
+            btnDelete.setOnAction((event -> tblCustomer.getItems().remove(param.getValue())));
+            return new ReadOnlyObjectWrapper<>(btnDelete);
+        });
+
+        tblCustomer.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, selectedValue) -> {
+            btnDelete.setDisable(selectedValue == null);
+            btnSave.setText(selectedValue == null ? "Save" : "Update");
+
+            txtId.setText(selectedValue.getId());
+            txtName.setText(selectedValue.getName());
+            txtAddress.setText(selectedValue.getAddress());
+            txtProPic.setText("Edit your profile picture");
+
+        });
     }
 
     private void initDataSource() {
         try {
             if (!Files.exists(sourcePath)) {
                 Files.createDirectory(sourcePath.getParent()); //create parent folder
-                Files.createFile(sourcePath); ////create files in parent folder
+                Files.createFile(sourcePath); //create files in parent folder
             }
 
             readSavedData();
@@ -58,6 +101,26 @@ public class MainInterfaceControl {
             new Alert(Alert.AlertType.ERROR, "Failed to create source file").showAndWait();
             Platform.exit();
         }
+    }
+
+    public void rbtMale_OnAction(ActionEvent event) {
+        imgProfile.setImage(new Image("assets/MaleIcon.png"));
+        defaultImage = Paths.get(String.valueOf(defaultImage));
+    }
+
+    public void rbtFemale_OnAction(ActionEvent event) {
+        imgProfile.setImage(new Image("assets/femaleIcon.png"));
+        defaultImage = Paths.get("assets/femaleIcon.png");
+
+    }
+    public void btnBrowse_OnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(
+          "Images","*jpeg","*.jpg","*.png","*.bmp"
+        ));
+        fileChooser.setTitle("Select your profile Image");
+        File file = fileChooser.showOpenDialog(btnBrowse.getScene().getWindow());
+        txtProPic.setText(file != null ? file.getAbsolutePath() : "No Image Data");
     }
 
     private void readSavedData() {
@@ -77,52 +140,73 @@ public class MainInterfaceControl {
         }
     }
 
-
     public void btnSave_OnAction(ActionEvent actionEvent) throws IOException {
-        if (!txtId.getText().matches("C\\d{3}") || tblCustomer.getItems().stream().anyMatch(c -> c.getId().equalsIgnoreCase(txtId.getText()))) {
-            txtId.requestFocus();
-            txtId.selectAll();
-            return;
 
-        } else if (txtName.getText().trim().isEmpty()) {
-            txtName.requestFocus();
-            txtName.selectAll();
-            return;
+        byte[] profilePicture = new byte[0];
+        if (btnSave.getText().equals("Save")) {
 
-        } else if (txtAddress.getText().trim().isEmpty()) {
-            txtAddress.requestFocus();
-            txtAddress.selectAll();
-            return;
+            if(!txtProPic.getText().trim().isEmpty()){
+                profilePicture = Files.readAllBytes(Paths.get(txtProPic.getText()));
+            }else {
+                profilePicture = Files.readAllBytes(defaultImage);
+            }
+
+
+            if (!txtId.getText().matches("C\\d{5}") || tblCustomer.getItems().stream().anyMatch(c -> c.getId().equalsIgnoreCase(txtId.getText()))) {
+                txtId.requestFocus();
+                txtId.selectAll();
+                return;
+
+            } else if (txtName.getText().trim().isEmpty()) {
+                txtName.requestFocus();
+                txtName.selectAll();
+                return;
+
+            } else if (txtAddress.getText().trim().isEmpty()) {
+                txtAddress.requestFocus();
+                txtAddress.selectAll();
+                return;
+            }
+
+            Customer customerObj = new Customer(
+                    txtId.getText(),
+                    txtName.getText(),
+                    txtAddress.getText(),
+                    profilePicture
+
+            );
+
+            tblCustomer.getItems().add(customerObj);
+            boolean result = storeData();
+
+            if (!result) {
+                new Alert(Alert.AlertType.ERROR, "Failed to Save").show();
+                tblCustomer.getItems().remove(customerObj);
+
+            } else {
+                txtId.clear();
+                txtName.clear();
+                txtAddress.clear();
+
+            }
+        } else if (btnSave.getText().equals("Update")) {
+
+
+            //update method run
+            Customer selectedCustomer = tblCustomer.getSelectionModel().getSelectedItem();
+            selectedCustomer.setProfilePic(profilePicture);
+            selectedCustomer.setId(txtId.getText());
+            selectedCustomer.setName(txtName.getText());
+            selectedCustomer.setAddress(txtAddress.getText());
+            tblCustomer.refresh();
         }
-
-
-        Customer customerObj = new Customer(
-                txtId.getText(),
-                txtName.getText(),
-                txtAddress.getText()
-        );
-        tblCustomer.getItems().add(customerObj);
-        boolean result = storeData();
-
-        if (!result) {
-            new Alert(Alert.AlertType.ERROR, "Failed to Save").show();
-            tblCustomer.getItems().remove(customerObj);
-
-        } else {
-            new Alert(Alert.AlertType.INFORMATION, "Saved Data Successfully").show();
-            txtId.clear();
-            txtName.clear();
-            txtAddress.clear();
-
-        }
-
     }
 
     private boolean storeData() {
         try {
             ObjectOutputStream oos =
                     new ObjectOutputStream(Files.newOutputStream(sourcePath, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING));
-            oos.writeObject(new ArrayList<Customer>(tblCustomer.getItems()));
+            oos.writeObject(new ArrayList<>(tblCustomer.getItems()));
             return true;
 
         } catch (IOException e) {
@@ -130,15 +214,17 @@ public class MainInterfaceControl {
             return false;
         }
 
-
     }
 
     public void btnReset_OnAction(ActionEvent actionEvent) {
+        txtProPic.clear();
         txtId.clear();
         txtName.clear();
         txtAddress.clear();
-        tblCustomer.getItems().clear();
+
     }
+
+
 
 }
 
--- src/main/java/lk/ijse/dep8/util/Customer.java
+++ src/main/java/lk/ijse/dep8/util/Customer.java
@@ -3,19 +3,21 @@ package lk.ijse.dep8.util;
 import java.io.Serializable;
 
 public class Customer implements Serializable {
+
     private String id;
     private String name;
     private String address;
+    private byte[] profilePic;
 
     public Customer() {
     }
 
-    public Customer(String id, String name, String address) {
+    public Customer(String id, String name, String address, byte[] profilePic) {
         this.setId(id);
         this.setName(name);
         this.setAddress(address);
+        this.setProfilePic(profilePic);
     }
-
 
     public String getId() {
         return id;
@@ -41,7 +43,15 @@ public class Customer implements Serializable {
         this.address = address;
     }
 
-    public void printData(){
-        System.out.printf("id= %s, name= %s, address= %s\n",id,name,address);
+    public byte[] getProfilePic() {
+        return profilePic;
     }
+
+    public void setProfilePic(byte[] profilePic) {
+        this.profilePic = profilePic;
+    }
+    public void printData(){
+        System.out.printf("id= %s, name= %s, address= %s\n", getId(), getName(), getAddress());
+    }
+
 }
